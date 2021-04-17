1fcd64e8a6b5947aa4ffa3f992a55e207eb82bc3
SuperRayss <59813844+SuperRayss@users.noreply.github.com>
2021-04-17 14:00:28 -0400

    Missed one

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ from libs.leaderboard import leaderboards, Leaderboard, lb_stats
 
 # Seq configuration
 seqlog.log_to_seq(
-    server_url="http://seq.pyre-bot.com:80",
+    server_url=f"{seq_url}:80",
     api_key=seq_api,
     level=log_level,
     batch_size=5,
